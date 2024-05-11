4dcc30eb76a80cee18159d05c86e819bd17ebe72
Fernando Aguilar <davidfer.aguilar02@gmail.com>
2024-05-11 02:49:56 -0500

    routes added

--- src/app.js
+++ src/app.js
@@ -1,14 +1,24 @@
 import express from 'express';
 import morgan from 'morgan';
+import taskRoutes from './routes/tasks.routes.js'
+import authRoutes from './routes/auth.routes.js'
+
+
 
 const app = express();
+
+// middlewares
 
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+// routes
 app.get('/' , (req, res) => res.json({message: "welcome to my API"}))
+app.use('/api',taskRoutes)
+app.use('/api', authRoutes)
 
+// error handler
 app.use((err, req, res, next) => {
     res.status(500).json({
         status: "error",
